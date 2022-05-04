d6ae890df80d8fff1b9aca58cf44001f4958f01e
Raihanul islam <97145938+Raihanulislam12s@users.noreply.github.com>
2022-05-04 09:37:16 -0700

    Create input.java

---
+++ input.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String args[])
+    {
+       //input 
+      Scanner sc=new Scanner(System.in);
+        int age= sc.nextInt();
+        String name=sc.nextLine();
+        double a=sc.nextDouble();
+        long b=sc.nextLong();
+        
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
