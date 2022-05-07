3ad5ab9e68fc27a2aad8f4321a3238b4ee3aba19
Raihanul islam <97145938+Raihanulislam12s@users.noreply.github.com>
2022-05-06 21:23:20 -0700

    Update if-else.java

--- if-else.java
+++ if-else.java
@@ -5,11 +5,14 @@ public class if_else1 {
         Scanner sc=new Scanner(System.in);
         int age=sc.nextInt();
         if (age>18){
-            System.out.println("adult");
+            System.out.println("Adult");
         }
         else{
-            System.out.println("Not adult");
+            System.out.println("Not Adult");
         }
 
     }
 }
+/*output
+Enter lassdan 18:Adult
+Enter 18 up:Not Adult
