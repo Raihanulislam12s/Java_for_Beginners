6f91bb59b4f7984bd6fbbc305ef50159c79246a0
Raihanul islam <97145938+Raihanulislam12s@users.noreply.github.com>
2022-05-06 21:14:52 -0700

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class for_loop {
+    public static void main(String args[]){
+       //input
+        //Scanner sc=new Scanner(System.in);
+       // int num=sc.nextInt();
+
+        // for loop
+        /*for (int i=0;i<11;i++){
+            System.out.println(i);
+        }*/
+
+        //while loop
+        /*int i=0;
+        while (i<11){
+            System.out.println(i);
+            i++;
+        }*/
+
+        //do while loop
+        /*int i=0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i<11);*/
+
+    }
+}
