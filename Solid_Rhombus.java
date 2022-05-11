7d9d1dce8fb1f0fdb08a2e605378e727fb016fba
Raihanul islam <97145938+Raihanulislam12s@users.noreply.github.com>
2022-05-11 20:30:51 +0600

    Create Solid_Rhombus.java

---
+++ Solid_Rhombus.java
@@ -0,0 +1,28 @@
+/*
+11.Solid Rhombus
+    --Output--
+        *****
+       *****
+      *****
+     *****
+    *****
+ */
+
+import java.util.*;
+
+public class Solid_Rhombus {
+    public static void main(String args[]){
+      int n=5;
+      //Spaces
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=n-i;j++){
+              System.out.print(" ");
+          }
+
+          for(int j=1;j<=n;j++){
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+    }
+}
